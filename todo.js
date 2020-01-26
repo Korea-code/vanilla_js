36962f79ca5f03d8785e1520cdbae1fdebb50d71
Korea-code <rlawlgy93@gmail.com>
2020-01-26 14:18:51 -0500

    todo list complete

--- todo.js
+++ todo.js
@@ -4,12 +4,31 @@ const todoForm = document.querySelector(".js-todoForm"),
 
 const TODO_LS = "todos";
 
-const todos = [];
+let todos = [];
+
+function deleteTodo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    todoList.removeChild(li);
+    const cleanTodos = todos.filter(function(todo){
+        return todo.id !== parseInt(li.id);
+    });
+    todos = cleanTodos;
+    saveToDos();
+
+}
+function saveToDos() {
+    localStorage.setItem(TODO_LS, JSON.stringify(todos));
+}
+
 function paintTodo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteTodo)
     const span = document.createElement("span");
+    const newId = todos.length + 1;
+    li.id = newId;
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -17,12 +36,19 @@ function paintTodo(text){
     todoInput.focus();
     const todoObj = {
         text: text,
-        id: todos.length + 1,
-    }
+        id: newId,
+    };
+    todos.push(todoObj);
+    saveToDos();
 }
+
 function loadTodos(){
-    const todos = localStorage.getItem(TODO_LS);
-    if(todos !== null){
+    const loadedTodos = localStorage.getItem(TODO_LS);
+    if(loadedTodos !== null){
+        const parsedTodos = JSON.parse(loadedTodos);
+        parsedTodos.forEach(function(todo){
+            paintTodo(todo.text);
+        });
     }
 
 }
